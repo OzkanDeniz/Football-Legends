26cb56adc0965a0db50fa4afec4414e12305b746
ozkan <ozkandnz09@gmail.com>
2024-08-29 20:36:43 +0300

    legendcontainer starter

--- starter/src/components/header/Header.jsx
+++ starter/src/components/header/Header.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import Container from 'react-bootstrap/esm/Container'
-import logo from "../../assets/logo.png"
-import Image from "react-bootstrap/Image"
+import React from "react";
+import Container from "react-bootstrap/esm/Container";
+import logo from "../../assets/logo.png";
+import Image from "react-bootstrap/Image";
+import headerStyle from "./Header.module.css";
 
 const Header = () => {
-  return(
+  return (
     <Container>
-    <Image src={logo} width={200} />
-<h1>Football Legends</h1>
-  </Container>
-    
-  ) 
-  
-}
+      <Image src={logo} width={200} />
+      <h1 className={`${headerStyle.title} my-2`}>Football Legends</h1>
+    </Container>
+  );
+};
 
-export default Header
+export default Header;
---
+++ starter/src/components/legends/LegendContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+
+
+const LegendContainer = () => {
+  return (
+    <Container>
+      LegendContainer
+    </Container>
+  )
+}
+
+export default LegendContainer
